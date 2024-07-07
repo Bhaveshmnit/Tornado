41831e41cbba40d8fafd936b2a03def1da776a6f
Bhaveshmnit <125796799+Bhaveshmnit@users.noreply.github.com>
2024-07-08 00:18:54 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiQwgrWnsw8pNRtStWTLXbR293T7crua8v_f7dfexmlfioY40f9coMkzzNFBDM9_Upag0&usqp=CAU";
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/";
 export const SWIGGY_URL2="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6550458&lng=77.1888201&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 export const SWIGGY_URL1= "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.693473&lng=77.297535&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 export const restroURL1="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6550458&lng=77.1888201&restaurantId="
